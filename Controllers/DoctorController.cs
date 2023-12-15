b6ecf1684fb5cdb6822937980cb5cdac2a325786
Florian Morina <florianmorina726@gmail.com>
2023-12-15 17:50:54 +0100

    Controller Completed

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Doktori.Interface;
+using Doktori.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,6 +22,53 @@ namespace Doktori.Controllers
         public IActionResult Index()
         {
             return View(_context.GetAllDoctors());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePost(DoctorEntity doctorData)
+        {
+            _context.Create(doctorData);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string Name)
+        {
+            var doctorDetails = _context.GetDoctorDetails(Name);
+            return View(doctorDetails);
+        }
+
+        [HttpPost]
+        public IActionResult EditPost(string _id, DoctorEntity doctorData)
+        {
+            _context.Update(_id, doctorData);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(string Name)
+        {
+            var doctorDetails = _context.GetDoctorDetails(Name);
+            return View(doctorDetails);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string Name)
+        {
+            var doctorDetails = _context.GetDoctorDetails(Name);
+            return View(doctorDetails);
+        }
+
+        [HttpPost]
+        public IActionResult DeletePost(string Name)
+        {
+            _context.Delete(Name);
+            return RedirectToAction("Index");
         }
     }
 }
