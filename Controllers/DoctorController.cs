fff1043afccf48a696b2495c67f993cf7d5258f5
Florian Morina <florianmorina726@gmail.com>
2023-12-15 16:38:29 +0100

    Doctor Controller Done

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Doktori.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,11 +11,16 @@ namespace Doktori.Controllers
 {
     public class DoctorController : Controller
     {
+        private readonly IDoctorsList _context;
 
+        public DoctorController(IDoctorsList context)
+        {
+            _context = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View(_context.GetAllDoctors());
         }
     }
 }
